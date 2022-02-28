4609bc83f2f94220995f71c658fb3076a610dec8
L1vePotato <dudarev.sasha2018@gmail.com>
2022-02-28 17:14:17 +0500

    first commit

---
+++ Testovie/Testovie/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Testovie
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //int A;
+            //int B;
+            //string X;
+            
+            Console.WriteLine("Введите число А");
+            int A = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите число B");
+            int B = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Режимы: Сложение +, вычитание -, умножение *, деление /");
+            string X = Convert.ToString(Console.ReadLine());
+            string Plus;
+            string Minus;
+            string Umnoj;
+            string Razdel;
+
+        }
+    }
+}
